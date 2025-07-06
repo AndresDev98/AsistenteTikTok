97c5211b0339438fb9922fc76fb201526bb12b31
Andres <desarrolladorapi@gmail.com>
2025-07-06 17:27:51 +0200

    Add Swagger :)

---
+++ AsistenteTikTok/Controllers/BotController.cs
@@ -0,0 +1,34 @@
+﻿using AsistenteTikTok.Application.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsistenteTikTok.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BotController : ControllerBase
+    {
+        private readonly BotCreationService _botCreator;
+
+        public BotController(BotCreationService botCreator)
+        {
+            _botCreator = botCreator;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateBot([FromQuery] string emulatorId = "127.0.0.1:5555")
+        {
+            var result = await _botCreator.CreateBotAsync(emulatorId);
+
+            if (result == null)
+                return BadRequest("Falló la creación del bot.");
+
+            return Ok(new
+            {
+                result.Username,
+                result.Email,
+                result.Status,
+                result.CreatedAt
+            });
+        }
+    }
+}
--- AsistenteTikTok/Program.cs
+++ AsistenteTikTok/Program.cs
@@ -1,34 +1,29 @@
+using AsistenteTikTok.Application.Interfaces;
+using AsistenteTikTok.Application.UseCases;
+using AsistenteTikTok.Infrastructure.Adb;
+using AsistenteTikTok.Infrastructure.Email;
+using AsistenteTikTok.Infrastructure.Files;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Inyección de dependencias
+builder.Services.AddSingleton<IEmailService, SecMailEmailService>();
+builder.Services.AddSingleton<IAdbService, AdbService>();
+builder.Services.AddSingleton<IBotAccountRepository, FileBotAccountRepository>();
+builder.Services.AddScoped<BotCreationService>();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
